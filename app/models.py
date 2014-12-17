22ce5fcc1e36bf9967875757f4c557c87b318601
bossip <baptiste.ossipow@gmail.com>
2014-12-17 11:25:05 +0100

    Follow me blindly.

--- app/models.py
+++ app/models.py
@@ -68,14 +68,11 @@ class User(db.Model):
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        return Post.query.join(
-            followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
-
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+    
     def __repr__(self):
         return '<User %r>' % (self.nickname)
-        
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
